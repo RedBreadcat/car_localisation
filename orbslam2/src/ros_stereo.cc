d38e7f72e3ca2601361c891b28e7fb551b0234e7
RedBreadcat <robysantoro@gmail.com>
2018-02-01 16:45:33 +1100

    SLAM runs and publishes

--- orbslam2/src/ros_stereo.cc
+++ orbslam2/src/ros_stereo.cc
@@ -4,32 +4,17 @@
 #include <chrono>
 #include <ros/ros.h>
 
-#include <cv_bridge/cv_bridge.h>
-#include <message_filters/subscriber.h>
-#include <message_filters/time_synchronizer.h>
-#include <message_filters/sync_policies/approximate_time.h>
-
 #include <opencv2/core/core.hpp>
 
 #include "System.h"
+#include "ImageGrabber.h"
 
 using namespace std;
 
-class ImageGrabber
-{
-public:
-    ImageGrabber(ORB_SLAM2::System* pSLAM):mpSLAM(pSLAM){}
-
-    void GrabStereo(const sensor_msgs::ImageConstPtr& msgLeft,const sensor_msgs::ImageConstPtr& msgRight);
-
-    ORB_SLAM2::System* mpSLAM;
-    bool do_rectify;
-    cv::Mat M1l,M2l,M1r,M2r;
-};
-
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "RGBD");
+    ros::init(argc, argv, "orbslam2");
+    ros::NodeHandle nh;
     ros::start();
 
     if(argc != 4)
@@ -42,7 +27,7 @@ int main(int argc, char **argv)
     // Create SLAM system. It initializes all system threads and gets ready to process frames.
     ORB_SLAM2::System SLAM(argv[1],argv[2],ORB_SLAM2::System::STEREO,true);
 
-    ImageGrabber igb(&SLAM);
+    ImageGrabber igb(nh, &SLAM);
 
     stringstream ss(argv[3]);
 	ss >> boolalpha >> igb.do_rectify;
@@ -86,17 +71,7 @@ int main(int argc, char **argv)
         cv::initUndistortRectifyMap(K_r,D_r,R_r,P_r.rowRange(0,3).colRange(0,3),cv::Size(cols_r,rows_r),CV_32F,igb.M1r,igb.M2r);
     }
 
-    ros::NodeHandle nh;
-
-    message_filters::Subscriber<sensor_msgs::Image> left_sub(nh, "/camera/left/image_raw", 1);
-    message_filters::Subscriber<sensor_msgs::Image> right_sub(nh, "/camera/right/image_raw", 1);
-    typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image> sync_pol;
-    message_filters::Synchronizer<sync_pol> sync(sync_pol(10), left_sub,right_sub);
-    sync.registerCallback(boost::bind(&ImageGrabber::GrabStereo,&igb,_1,_2));
-
     ros::spin();
-
-    // Stop all threads
     SLAM.Shutdown();
 
     // Save camera trajectory
@@ -108,54 +83,3 @@ int main(int argc, char **argv)
 
     return 0;
 }
-
-void ImageGrabber::GrabStereo(const sensor_msgs::ImageConstPtr& msgLeft,const sensor_msgs::ImageConstPtr& msgRight)
-{
-    // Copy the ros image message to cv::Mat.
-    cv_bridge::CvImageConstPtr cv_ptrLeft;
-    try
-    {
-        cv_ptrLeft = cv_bridge::toCvShare(msgLeft);
-    }
-    catch (cv_bridge::Exception& e)
-    {
-        ROS_ERROR("cv_bridge exception: %s", e.what());
-        return;
-    }
-
-    cv_bridge::CvImageConstPtr cv_ptrRight;
-    try
-    {
-        cv_ptrRight = cv_bridge::toCvShare(msgRight);
-    }
-    catch (cv_bridge::Exception& e)
-    {
-        ROS_ERROR("cv_bridge exception: %s", e.what());
-        return;
-    }
-
-    cv::Mat transformation;
-    if (do_rectify)
-    {
-        cv::Mat imLeft, imRight;
-        cv::remap(cv_ptrLeft->image,imLeft,M1l,M2l,cv::INTER_LINEAR);
-        cv::remap(cv_ptrRight->image,imRight,M1r,M2r,cv::INTER_LINEAR);
-        transformation = mpSLAM->TrackStereo(imLeft,imRight,cv_ptrLeft->header.stamp.toSec());
-    }
-    else
-    {
-        transformation = mpSLAM->TrackStereo(cv_ptrLeft->image,cv_ptrRight->image,cv_ptrLeft->header.stamp.toSec());
-    }
-
-    //TODO: Publish transformation
-
-    //possibilities:
-    //http://docs.ros.org/api/geometry_msgs/html/msg/Pose.html
-    //http://docs.ros.org/api/geometry_msgs/html/msg/PoseStamped.html
-    //http://docs.ros.org/api/geometry_msgs/html/msg/PoseWithCovariance.html
-    //http://docs.ros.org/api/geometry_msgs/html/msg/PoseWithCovarianceStamped.html
-
-    //need to turn the matrix into a quaternion. eigen?
-}
-
-
